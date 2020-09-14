60744433098a288f9f4ec1f7caa519a44ed09c3f
farshad68 <farshad.afshari68@gmail.com>
2020-09-14 11:56:46 +0200

    initiate service layer with spring boot

---
+++ service/src/main/java/org/dice/FROCKG/FrockgApplication.java
@@ -0,0 +1,13 @@
+package org.dice.FROCKG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrockgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrockgApplication.class, args);
+	}
+
+}
